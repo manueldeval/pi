d6dd5705745d4303249a0254d09d49649173cd97
Manuel Deval <m.deval@actongroup.com>
2024-07-03 16:05:39 +0200

    (WIP) Initial commit

---
+++ myserver/src/main.rs
@@ -0,0 +1,23 @@
+use std::env;
+
+use axum::{response::IntoResponse, routing::get, Json, Router};
+
+pub async fn status_handler() -> impl IntoResponse {
+
+    let json_response = serde_json::json!({
+        "status": "success",
+        "message": "Pi is ok."
+    });
+    Json(json_response)
+}
+
+#[tokio::main]
+pub async fn main() {
+    let key = "PORT";
+    let port = env::var(key).unwrap_or("3000".to_string());
+
+    println!("Server started successfully on port {}",port);
+    let route = Router::new().route("/api/status", get(status_handler));
+    let listener = tokio::net::TcpListener::bind(format!("0.0.0.0:{}",port)).await.unwrap();
+    axum::serve(listener, route).await.unwrap();
+}
